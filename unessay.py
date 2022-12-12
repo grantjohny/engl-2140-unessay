7f7b9e311de09606223939f0e0878a3208cfa47c
John grant <johngrant@MacBook-Air.attlocal.net>
2022-12-12 18:05:06 -0500

    initial commit

---
+++ unessay.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/AfricanAmericanAuthors")
+def aaauthors():
+	return render_template("aaauthors.html")
+
+@app.route("/PuritanAuthors")
+def pauthors():
+	return render_template("pauthors.html")
+
+@app.route("/About")
+def about():
+	return render_template("about.html")
+	
+if __name__ == "__main__":
+	app.run(debug=True)
